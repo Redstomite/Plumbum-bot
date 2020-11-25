5068ab6eb3078aeeabaebb4ddcc0ec2192e27a82
Rahul Prabhu <rahul@grokwithrahul.com>
2020-11-25 19:15:59 +0530

    Improved warn functionality
    +ability to clear warnings
    +now shows full info about warns.

--- db_queries.py
+++ db_queries.py
@@ -1,6 +1,6 @@
 import os
 import tinydb
-
+from datetime import datetime
 
 class dbquery:
     def __init__(self):
@@ -11,10 +11,11 @@ class dbquery:
         if not os.path.exists(newpath):
             os.makedirs(newpath)
 
-    def warnUser(self, server_name, user_id, reason):
+    def warnUser(self, server_name, user_id, reason, warner):
         self.DBCheck(server_name)
         db = tinydb.TinyDB("./database/" + server_name + "/warnings.json")
-        db.insert({"username": user_id, "reason": reason})
+        now = datetime.now()
+        db.insert({"username": user_id, "reason": reason, "warner": warner, "time": now.strftime("%m/%d/%Y, %H:%M:%S")})
 
     def getWarnings(self, server_name, user_id):
         Query = tinydb.Query()
@@ -22,4 +23,9 @@ class dbquery:
         db = tinydb.TinyDB("./database/" + server_name + "/warnings.json")
         warnings = db.search(Query.username == user_id)
         numWarnings = len(warnings)
-        return numWarnings
+        return numWarnings, warnings
+
+    def clearwarns(self, server_name, user_id):
+        db = tinydb.TinyDB("./database/" + server_name + "/warnings.json")
+        Query = tinydb.Query()
+        db.remove(Query.username == user_id)
--- main.py
+++ main.py
@@ -43,10 +43,11 @@ async def setup(ctx, type):
 
 @bot.command(name='warn', help='warns a user')
 @has_permissions(administrator=True)
-async def warn(ctx, user: str, *args):
+async def warn(ctx, user: discord.Member, *args):
     reason = " ".join(args[:])
     h = dbquery()
-    h.warnUser(ctx.message.guild.name, user, reason)
+    warner = ctx.message.author
+    h.warnUser(ctx.message.guild.name, user, reason, warner)
     response = user + " has been warned for (" + reason + "). Don't be a bad boi."
     await ctx.send(response)
 
@@ -66,11 +67,17 @@ async def warn_error(error, ctx):
 
 
 @bot.command(name='warnings', help="show all warnings a user has")
-async def warn(ctx, usr: str):
+async def warnings(ctx, usr: discord.Member):
     h = dbquery()
-    numwarned = h.getWarnings(ctx.message.guild.name, usr)
-    response = usr + " has been warned " + str(numwarned) + " times so far."
+    numwarned, info = h.getWarnings(ctx.message.guild.name, str(usr))
+    response = usr + " has been warned " + str(numwarned) + " times so far. Here is why:" + info
     await ctx.send(response)
+
+
+@bot.command(name='clearwarns')
+async def clearwarns(ctx, user: discord.Member):
+    h = dbquery()
+    h.warnUser(ctx.message.guild.name)
 
 
 @bot.command(name='kick')
@@ -79,6 +86,13 @@ async def kick(ctx, kicked: discord.Member, *, reason=None):
     print(kicked)
     await kicked.kick(reason=reason)
     response = kicked + "has been kicked"
+    await ctx.send(response)
+
+
+@bot.command(name='ban')
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, kicked: discord.Member):
+    response = str(kicked) + " has been banned"
     await ctx.send(response)
 
 bot.run(TOKEN)
